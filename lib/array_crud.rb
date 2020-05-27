eb6fa173a575380c18d1b97b4f278af9ddc9857b
Aian Soktoev <soktoevayan@gmail.com>
2020-05-27 16:29:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return b = array.shift()
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return c = array.pop()
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array.index(index_number) = element 
 end
